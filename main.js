b2d4bd902a4682f876d3f1dae799d9ddde6faa2b
Natalia Reggiani <nreggiani77@gmail.com>
2022-05-31 18:36:39 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// let portfolio = document.querySelector('#port')
+// let imagen = document.querySelector('.imagen')
+// portfolio.addEventListener('click', function(){
+//     imagen.style.display = 'none'
+// })
