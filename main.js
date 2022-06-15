b2a18c470e00f6c13a24f34fcd8368e699b70d7f
Natalia Reggiani <nreggiani77@gmail.com>
2022-06-15 19:06:52 -0300

    evento scroll

--- main.js
+++ main.js
@@ -4,3 +4,11 @@
 //     imagen.style.display = 'none'
 // })
 
+
+
+window.addEventListener('scroll', function(){
+    let prueba = document.querySelector('#prueba')
+    let h = document.querySelectorAll('h3')
+    prueba.innerHTML = ` <h3>Portfolio</h3>`
+   console.log('scrolling')
+})
