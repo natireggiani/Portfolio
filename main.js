e04e83091a3eb4b61f03cc2e5fabccfe2b94cdb1
Natalia Reggiani <nreggiani77@gmail.com>
2022-06-19 09:10:35 -0300

    navbar

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ window.addEventListener('scroll', function(){
     prueba.classList.remove('hide')
     // prueba.innerHTML = ` <h3>Portfolio</h3>`
 })
+
+let menu = document.querySelector('.menu')
+let ul = document.querySelector('#lista-menu')
+let header = document.querySelector('#nav')
+
+menu.addEventListener('click', function(){
+    ul.classList.toggle('none')
+    header.classList.toggle('header')
+})
